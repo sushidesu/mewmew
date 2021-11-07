c87b474b5e6e18309e1c95edd94ead4727c47709
sushidesu <hugemerge@gmail.com>
2021-11-07 20:50:57 +0900

    parse json and return it

--- api/send-message.go
+++ api/send-message.go
@@ -6,11 +6,29 @@ import (
 )
 
 type sendMessageResponse struct {
-	Message string `json:"message"`
+	Message string                 `json:"message"`
+	Body    map[string]interface{} `json:"body"`
 }
 
 func SendMessageHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "invalid method", http.StatusBadRequest)
+		return
+	}
+	if r.Header.Get("Content-Type") != "application/json" {
+		http.Error(w, "invalid Content-Type", http.StatusBadRequest)
+		return
+	}
+
+	var jsonBody map[string]interface{}
+	err := json.NewDecoder(r.Body).Decode(&jsonBody)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
 	json.NewEncoder(w).Encode(sendMessageResponse{
 		Message: "hello",
+		Body:    jsonBody,
 	})
 }
