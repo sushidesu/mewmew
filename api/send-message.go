548647d066c924e18326ff54210a79d1f4f2c44d
sushidesu <hugemerge@gmail.com>
2021-11-07 21:27:20 +0900

    send message to slack

--- api/send-message.go
+++ api/send-message.go
@@ -1,16 +1,25 @@
 package handler
 
 import (
+	"bytes"
 	"encoding/json"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 type sendMessageResponse struct {
-	Message string                 `json:"message"`
-	Body    map[string]interface{} `json:"body"`
+	Message string `json:"message"`
 }
 
 func SendMessageHandler(w http.ResponseWriter, r *http.Request) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
 	if r.Method != "POST" {
 		http.Error(w, "invalid method", http.StatusBadRequest)
 		return
@@ -21,14 +30,42 @@ func SendMessageHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	var jsonBody map[string]interface{}
-	err := json.NewDecoder(r.Body).Decode(&jsonBody)
+	err = json.NewDecoder(r.Body).Decode(&jsonBody)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
+	// send message to slack (#message-from-mewmew)
+	webhook_url := os.Getenv("WEBHOOK_URL_MESSAGE_FROM_MEWMEW")
+
+	// build message body
+	body, err := json.Marshal(jsonBody)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	type SlackSendMessage struct {
+		Text string `json:"text"`
+	}
+	slack_message := SlackSendMessage{
+		Text: string(body),
+	}
+	message, err := json.Marshal(slack_message)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// send request
+	res, err := http.NewRequest("POST", webhook_url, bytes.NewBuffer(message))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer res.Body.Close()
+
 	json.NewEncoder(w).Encode(sendMessageResponse{
-		Message: "hello",
-		Body:    jsonBody,
+		Message: "ok",
 	})
 }
